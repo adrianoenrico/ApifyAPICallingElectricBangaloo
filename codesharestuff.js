cd9160c12a64ad94b03abd068434ef048aa297c4
Adriano Alecrim <adrianoenrico1@gmail.com>
2021-05-11 17:46:13 +0100

    fixed syntax errors

--- codesharestuff.js
+++ codesharestuff.js
@@ -19,35 +19,53 @@
 // - Send the data back to the same API URL from where you downloaded the data,
 //   the new array (with cheapest offers) should be send as the body/payload of the request.
 
-import axios from 'axios'
-
-
-const requestProducts = async () => {
-    // let offers = await axios.get('https://api.apify.com/v2/datasets/Gv6MxHyGzvPbchfLI/items?clean=true&format=json')
-    let offers = [
-        {
-            "productId": "dgdgfdfg",
-            "offerId": "545g43g34g",
-            "price": "$4.25"
-        },
-        {
-            "productId": "dgdgfdfg",
-            "offerId": "545g43g34g",
-            "price": "$4.25"
-        },
-    ]
+// import axios from 'axios'
+let offers = [
+    {
+        "productId": "dgdgfdfg",
+        "offerId": "545g43g34g",
+        "price": "$4.25"
+    },
+    {
+        "productId": "dgdgfdfg",
+        "offerId": "545g12343g34g",
+        "price": "$4.12"
+    },
+    {
+        "productId": "dgdgfdfg123",
+        "offerId": "545g1234543g34g",
+        "price": "$44.00"
+    },
+    {
+        "productId": "dgdgfdfg123",
+        "offerId": "51245g1212334543g34g",
+        "price": "$40.00"
+    },
+]
+const getBestOffers = async (offers) => {
+    try{
+            // let offers = await axios.get('https://api.apify.com/v2/datasets/Gv6MxHyGzvPbchfLI/items?clean=true&format=json')
     let products = offers.map(e => e.productId)
-    products = Array(new Set(...products))
+    products = Array(... new Set(products))
+    console.log("prods", products);
     let sortedOffers = []
-    for (prod of products) {
-        let sameProd = []
-        for (offer of offers) {
+    for (let prod of products) {
+        console.log("Current prod: ",prod)
+        let sameProd = [];
+        for (let offer of offers) {
             if (offer.productId == prod) {
                 sameProd.push(offer)
             }
         }
-        sameProd.sort((a, b) => Number(a.price) - Number(b.price))
+        sameProd.sort((a, b) => Number(a.price.replace('$','')) - Number(b.price.replace('$','')))
         sortedOffers.push(sameProd[0])
     }
-    //   axios.post("https://api.apify.com/v2/datasets/Gv6MxHyGzvPbchfLI/items?clean=true&format=json", sortedOffers)
+    // axios.post("https://api.apify.com/v2/datasets/Gv6MxHyGzvPbchfLI/items?clean=true&format=json", sortedOffers)
+    return sortedOffers
+    }catch(ex){
+        console.log(ex);
+        return;
+    }
 }
+
+console.log(getBestOffers(offers));
